d79de9844d721d761ece8920bbb5002079cb4b1a
spadr <ennbunn1626@yahoo.co.jp>
2021-12-24 11:19:24 +0000

    データの成型

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,81 @@
 import React, { Component } from "react";
-import Chart from "react-apexcharts";
 import Config from "./config";
-import { getPrefecture, getPopulation } from "./apiFetch";
+
+interface _Prefecture {
+  message: string;
+  result: [
+    {
+      prefCode: number;
+      prefName: string;
+    }
+  ];
+}
+
+interface _Population {
+  message: string;
+  result: [
+    {
+      boundaryYear: number;
+      data: [
+        {
+          label: string;
+          data: [
+            {
+              year: number;
+              value: number;
+            }
+          ];
+        }
+      ];
+    }
+  ];
+}
+
+interface _State {
+  series: [
+    {
+      show: boolean;
+      name: string;
+      code: number;
+      data: [
+        {
+          x: number;
+          y: number;
+        }
+      ];
+    }
+  ];
+  xaxis: {
+    type: string;
+    title: string;
+  };
+  yaxis: {
+    type: string;
+    title: string;
+  };
+  boundaryYear: number;
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
+      series: [],
       options: {
         chart: {
-          id: "basic-line",
+          id: "population-line",
+        },
+        legend: {
+          position: "right",
+        },
+        yaxis: {
+          title: {
+            text: Config.plotLabelY,
+          },
         },
         xaxis: {
           title: {
-            text: "年度",
+            text: Config.plotLabelX,
           },
           type: "datetime",
           labels: {
@@ -23,56 +84,84 @@ class App extends Component {
             },
           },
         },
-        yaxis: {
-          title: {
-            text: "総人口",
-          },
-        },
       },
-      series: [
-        {
-          show: true,
-          name: "東京都",
-          code: 13,
-          data: [
-            { x: "1/1/1990", y: 0 },
-            { x: "1/1/2000", y: 10 },
-            { x: "1/1/2010", y: 30 },
-          ],
-        },
-        {
-          show: true,
-          name: "大阪府",
-          code: 15,
-          data: [
-            { x: "1/1/1990", y: 0 },
-            { x: "1/1/2000", y: 100 },
-            { x: "1/1/2010", y: 300 },
-          ],
-        },
-      ],
     };
   }
 
+  componentDidMount() {
+    this.getPrefecture();
+    this.getPopulation(Number(Config.endPointPopulationParameter1Value));
+  }
+
+  async getPrefecture() {
+    const res1: Response = await fetch(Config.endPointPrefecture, {
+      headers: { "X-API-KEY": Config.apiKey },
+    });
+
+    if (res1.ok) {
+      const data = (await res1.json()) as _Prefecture;
+      const pref = data.result.slice();
+      const init = [];
+
+      for (let i = 0; i < pref.length; i++) {
+        const tpm_init = {
+          show: false,
+          name: pref[i].prefName,
+          code: pref[i].prefCode,
+          data: [],
+        };
+        init.push(tpm_init);
+      }
+      this.setState({
+        series: init,
+      });
+    } else {
+      console.error("Could not GET Prefecture data");
+    }
+  }
+
+  async getPopulation(index: number) {
+    const prefUrl =
+      Config.endPointPopulation +
+      "?" +
+      Config.endPointPopulationParameter1 +
+      "=" +
+      index + //都道府県コードは1から
+      "&" +
+      Config.endPointPopulationParameter2 +
+      "=" +
+      Config.endPointPopulationParameter2Value;
+
+    const res2: Response = await fetch(prefUrl, {
+      headers: { "X-API-KEY": Config.apiKey },
+    });
+
+    if (res2.ok) {
+      const data = (await res2.json()) as _Population;
+      const series_copy = this.state.series.slice();
+      const init = [];
+      Object.keys(data.result.data[0].data).forEach((i) => {
+        const tpm = {
+          x: data.result.data[0].data[i].year,
+          y: data.result.data[0].data[i].value,
+        };
+        init.push(tpm);
+      });
+      series_copy[index - 1].data = init; //配列は0から
+      series_copy[index - 1].show = !series_copy[index - 1].show; //配列は0から
+      this.setState({
+        series: series_copy,
+      });
+      console.log(series_copy);
+    } else {
+      console.error("Could not GET Population data");
+    }
+  }
+
   render() {
-    console.log("Prefecture");
-    console.log(getPrefecture());
-    console.log("Population");
-    console.log(getPopulation());
-    console.log("apiKey");
-    console.log(Config.apiKey);
     return (
-      <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="line"
-              width="500"
-            />
-          </div>
-        </div>
+      <div>
+        <h1>Title</h1>
       </div>
     );
   }
